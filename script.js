c6462ecfe61031f986b4fd92c57e0c5be2eaa02c
oravihzer <oravihzer@walla.com>
2024-11-14 19:22:23 +0200

    PORTFOLIO2024

---
+++ script.js
@@ -0,0 +1,26 @@
+function greetFn() {
+  let userNameInput = document.getElementById("userNameInput");
+
+  document.getElementById("resultSpan").innerHTML = "Loading.";
+
+  setTimeout(() => {
+    document.getElementById("resultSpan").innerHTML = "Loading..";
+  }, 1000);
+  setTimeout(() => {
+    document.getElementById("resultSpan").innerHTML = "Loading...";
+  }, 2000);
+
+  setTimeout(() => {
+    document.getElementById(
+      "resultSpan"
+    ).innerHTML = `function greeting(name)  {
+    console.log("Hello World " + name);
+  }
+    <br>
+  greeting(name)`;
+  }, 3000);
+  setTimeout(() => {
+    alert(`Welcome ${userNameInput.value}, So happy to see you :)`);
+    document.getElementById("resultSpan").innerHTML = "";
+  }, 4000);
+}
