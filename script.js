3e44f4729ebd73c1d3a8d9ebf43f4d0a7208d6d9
oravihzer <oravihzer@walla.com>
2024-12-07 13:04:54 +0200

    Fixed Navbar

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ function greetFn() {
     document.getElementById("resultSpan").innerHTML = "";
   }, 4000);
 }
+
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
